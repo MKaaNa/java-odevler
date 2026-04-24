3a171dae687598caba2823f425785155bbf3fef8
kaan <kaan.altin@basarimobile.com>
2026-04-24 17:53:49 +0300

    Add homework 11 max min number finder

---
+++ odev-11-en-buyuk-en-kucuk/EnBuyukEnKucuk.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EnBuyukEnKucuk {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int n = scanner.nextInt();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + ". Sayıyı giriniz: ");
+            int val = scanner.nextInt();
+            if (val > max) max = val;
+            if (val < min) min = val;
+        }
+        System.out.println("En büyük sayı: " + max);
+        System.out.println("En küçük sayı: " + min);
+        scanner.close();
+    }
+}
