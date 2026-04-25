1ba16bfd786976d81bff486d6e301c14c23fc84c
Kaan <mkaanaltin@gmail.com>
2026-04-26 00:27:55 +0300

    Add homework 48 frequency count

---
+++ odev-48-frekans/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 20, 10, 10, 20, 5, 20};
+        boolean[] printed = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (printed[i]) continue;
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] == arr[i]) {
+                    count++;
+                    printed[j] = true;
+                }
+            }
+            System.out.println(arr[i] + " sayısı " + count + " kere tekrar edildi.");
+        }
+    }
+}
